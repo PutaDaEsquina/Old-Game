f444a4e32c5923cc1ba06a568addc2e4d92be55f
jhonatasgaragnani <virtualdjhonata@hotmail.com>
2018-10-03 19:47:02 -0300

    Commit Wesley

--- jogo.js
+++ jogo.js
@@ -22,14 +22,14 @@ $(document).ready( function(){
 	$('#btn_iniciar_jogo').click( function(){
 
 		//valida a digitaçao dos apelidos 
-		if($('#entrada_apelido_jogador_1').val() == ''){
+		/*if($('#entrada_apelido_jogador_1').val() == ''){
 			alert('Apelido do jogador 1 não foi digitado');
 			return false;
 		}
 		if($('#entrada_apelido_jogador_2').val() == ''){
 			alert('Apelido do jogador 2 não foi digitado');
 			return false;
-		}
+		}*/
 
 		//exibir apelido dos jogadores escolhidos
 		$('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
